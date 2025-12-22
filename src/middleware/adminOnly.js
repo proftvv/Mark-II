f10232cb89c127c5b892a62c83811f281be3a7f7
Proftvv <ozcanyilmazcelebi2016@gmail.com>
2025-12-22 19:11:43 +0300

    fix: Disable IP check for admin routes on Vercel

--- src/middleware/adminOnly.js
+++ src/middleware/adminOnly.js
@@ -2,7 +2,8 @@ const { sendError } = require('../utils/errorCodes');
 const { isLocalIp, logAdminAction } = require('../utils/roleValidation');
 
 function adminOnly(req, res, next) {
-  if (!isLocalIp(req)) {
+  // Skip IP check on Vercel - rely on authentication instead
+  if (!process.env.VERCEL && !isLocalIp(req)) {
     logAdminAction(req, 'UNAUTHORIZED_IP_ACCESS', { 
       endpoint: req.path,
       method: req.method
